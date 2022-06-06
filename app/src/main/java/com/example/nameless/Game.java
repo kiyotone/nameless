5c0f6bf6ab37c4e7f8ee1e5f1852eb77b211b8a3
kiyotone <kunwarkirtan36@gmail.com>
2022-06-06 15:16:53 +0545

    gameloop

---
+++ app/src/main/java/com/example/nameless/Game.java
@@ -0,0 +1,77 @@
+package com.example.nameless;
+
+import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.app.Activity;
+
+import androidx.core.content.ContextCompat;
+
+class Game extends SurfaceView implements SurfaceHolder.Callback {
+    private GameLoop gameLoop;
+    private Context context;
+
+    public Game(Context context) {
+        super(context);
+
+
+
+        SurfaceHolder  surfaceHolder = getHolder();
+        surfaceHolder.addCallback(this);
+
+        this.context = context;
+        gameLoop = new com.example.nameless.GameLoop(this,surfaceHolder);
+
+        setFocusable(false);
+
+    }
+    @Override
+    public void surfaceCreated(SurfaceHolder holder){
+        gameLoop.startLoop();
+
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder,int format,int width,int height){
+
+
+    }
+
+    @Override
+    public  void surfaceDestroyed(SurfaceHolder holder){
+
+    }
+
+    @Override
+    public void draw(Canvas canvas) {
+        super.draw(canvas);
+        drawUPS(canvas);
+        drawFPS(canvas);
+    }
+
+    public void drawUPS(Canvas canvas){
+        String averageUPS = Double.toString(gameLoop.getAverageUPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context,R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("UPS :"+ averageUPS,100,50,paint);
+    }
+
+
+    public void drawFPS(Canvas canvas){
+        String averageFPS = Double.toString(gameLoop.getAverageFPS());
+        Paint paint = new Paint();
+        int color = ContextCompat.getColor(context,R.color.magenta);
+        paint.setColor(color);
+        paint.setTextSize(50);
+        canvas.drawText("FPS :"+ averageFPS,100,100,paint);
+    }
+
+    public void update() {
+    }
+}
